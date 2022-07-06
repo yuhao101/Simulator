cdab0c1c28859523eb2dba149300a9986f0dc016
yuhao101 <yuhao101@connect.hku.hk>
2022-07-06 11:08:23 +0800

    u

--- simulator_evaluation/path.py
+++
@@ -1,5 +0,0 @@
-
-# load_path = "E:\\simulator\\"
-# code_path = "E:\\simulator\\"
-load_path = "D:/Simon_NEW/civil/simulator-matching-function-validation-main/"
-code_path = "D:/Simon_NEW/civil/simulator-matching-function-validation-main/"
